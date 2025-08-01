ee437c0aed63f2647e329d614e07a2463fa1b5fc
Prakash Singh Chauhan <pchauhan@Prakashs-MacBook-Pro.local>
2025-08-01 08:05:08 +0530

    add new project next-auth and deleted e-comm-app project

--- e-comm-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const HomePage = () => {
-  return (
-    <div className="text-2xl flex  justify-center align-center">
-      Hello there! Welcome to this e-commerce store.
-      <hr></hr>
-    </div>
-  );
-};
-export default HomePage;
